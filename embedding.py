21dede9dc1f5ef27e447df222b9ef7a5e2c7dff8
Dacon-official <phykn.kr@gmail.com>
2023-04-27 20:46:45 +0900

    conv1d -> linear

--- embedding.py
+++ embedding.py
@@ -7,17 +7,17 @@ class numEmbedding(nn.Module):
         super().__init__()
         self.embedding_dim = embedding_dim
         self.nan = nn.Parameter(torch.randn(embedding_dim))
-        self.linear = nn.Conv1d(1, embedding_dim, 1)
+        self.linear = nn.Linear(1, embedding_dim)
 
     def forward(self, x):
         b, n = x.shape
 
         mask = torch.isnan(x).float()
-        mask = repeat(mask, "b n -> b c n", c = self.embedding_dim)
+        mask = repeat(mask, "b n -> b n d", d = self.embedding_dim)
 
         x = torch.nan_to_num(x)
-        x = rearrange(x, "b n -> b 1 n")
+        x = rearrange(x, "b n -> b n 1")
         x = self.linear(x)
 
-        nan = repeat(self.nan, "c -> b c n", b = b, n = n)
+        nan = repeat(self.nan, "d -> b n d", b = b, n = n)
         return x * (1 - mask) + nan * mask
